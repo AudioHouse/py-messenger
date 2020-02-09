72206f338f1c26f5ff750767e2aead81ed5e4620
AudioHouse <austinmiller360@gmail.com>
2020-02-09 09:37:12 +0000

    Implement receiving SMS messages

--- server.py
+++ server.py
@@ -1,20 +1,28 @@
 from flask import Flask, request
-from twilio import twiml
-
 from py_messenger import sms_client
 
 app = Flask(__name__)
 
 
-@app.route('/sms', methods=['POST'])
-def sms():
+@app.route('/', methods=['GET'])
+def health_check():
+    return str('Hey, this is py-messenger!')
+
+
+@app.route('/inbound', methods=['POST'])
+def inbound_sms():
     number = request.form['From']
     message_body = request.form['Body']
     print(f'Received a message from {number}: {message_body}')
+    return str('OK')
 
-    # resp = twiml.Response()
-    # resp.message('Hello {}, you said: {}'.format(number, message_body))
-    # return str(resp)
+
+@app.route('/outbound', methods=['POST'])
+def outbound_sms():
+    destination_number = request.json['destination']
+    message = request.json['message']
+    sms_client.send_sms_message(destination_number, message)
+    return str('OK')
 
 
 if __name__ == '__main__':
