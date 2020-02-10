59d7fda56a02108a0850f1daa74d5b1ea122c25a
Austin Miller <admiller@sandiego.edu>
2020-02-09 20:31:08 -0800

    add status codes

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 from py_messenger import sms_client
 
 app = Flask(__name__)
@@ -14,7 +14,7 @@ def inbound_sms():
     number = request.form['From']
     message_body = request.form['Body']
     print(f'Received a message from {number}: {message_body}')
-    return str('OK')
+    return Response(status=204)
 
 
 @app.route('/outbound', methods=['POST'])
@@ -22,7 +22,7 @@ def outbound_sms():
     destination_number = request.json['destination']
     message = request.json['message']
     sms_client.send_sms_message(destination_number, message)
-    return str('OK')
+    return Response(status=204)
 
 
 if __name__ == '__main__':
