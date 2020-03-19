cf06d1ed9f39110ef2c5a97ba9e454bd23aa90ab
AudioHouse <austinmiller360@gmail.com>
2020-03-19 04:48:36 +0000

    Add run script

--- health_kit/scheduler.py
+++ health_kit/scheduler.py
@@ -24,7 +24,6 @@ def check_active_range():
     today9am = now.replace(hour=9, minute=0, second=0, microsecond=0)
     today11pm = now.replace(hour=23, minute=30, second=0, microsecond=0)
     if (now > today9am) and (now < today11pm):
-        print(get_zero_time())
         return True
     else:
         return False
